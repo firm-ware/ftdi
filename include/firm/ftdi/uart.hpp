752ecca2dc3efaff3127d5052359185d1ec5751b
Joseph Paul <joseph@sehrgute.software>
2024-03-13 14:07:28 +0100

    Switch to polling model

--- include/firm/ftdi/uart.hpp
+++ include/firm/ftdi/uart.hpp
@@ -12,19 +12,65 @@ namespace ftdi {
  *
  * @note Before using any other methods of this class, first, the `open()` and then
  *       the `configure()` method must be called to set the initial configuration.
+ *
+ * @note Due to the polled nature of the USB protocol, and the callback-based interface
+ *       of the UART class, it is required to regularly call the `poll()` method in
+ *       order to check the FTDI device for new data in its RX buffer.
  */
 class FTDI : public firm::uart::UART {
 public:
-    FTDI(firm::logging::Logger *log) : log(log), handler(NULL), rxEnabled(false) {
+    /**
+     * Construct a new FTDI object
+     *
+     * @param rxBuffer Pointer to a buffer where received data will be stored
+     * @param rxBufferSize Size of the receive buffer
+     * @param log Pointer to a logger instance
+     */
+    FTDI(uint8_t *rxBuffer, size_t rxBufferSize, firm::logging::Logger *log)
+        : rxBuffer(rxBuffer), rxBufferSize(rxBufferSize), log(log), handler(NULL), rxEnabled(false) {
         ftdi_init(&this->ctx);
     };
+
     ~FTDI() {
         ftdi_usb_close(&this->ctx);
         ftdi_deinit(&this->ctx);
     };
+
+    /**
+     * Establish a connection to the FTDI device with the given VID and PID.
+     *
+     * @param vid
+     * @param pid
+     * @return int
+     */
     int open(uint16_t vid, uint16_t pid);
+
+    /**
+     * Configure the UART interface with the given parameters.
+     *
+     * @param config
+     * @return int
+     */
     int configure(firm::uart::Config_t config);
+
+    /**
+     * Set the baud rate of the UART interface.
+     *
+     * @param rate
+     */
     void setBaudRate(uint32_t rate);
+
+    /**
+     * Poll the FTDI for new data or signals in the RX buffer.
+     *
+     * If the FTDI reports a break, frame or parity error, the `uartSignal()` method
+     * of the registered handler is called with the corresponding signal type. If
+     * data is available, the `uartData()` method of the handler will be called.
+     *
+     * If RX is disabled, this method will do nothing.
+     */
+    void poll();
+
     void enableRx(bool enabled);
     void enableTx(bool enabled);
     void sendByte(uint8_t value);
@@ -33,10 +79,12 @@ public:
     void sendBreak(uint16_t durationUs);
     void registerHandler(firm::uart::Handler *handler);
 
-    static int streamCallback(uint8_t *buffer, int length, FTDIProgressInfo *progress, void *userdata);
-
 private:
     struct ftdi_context ctx;
+
+    uint8_t *rxBuffer;
+    size_t rxBufferSize;
+
     firm::logging::Logger *log;
     firm::uart::Handler *handler;
 
@@ -47,17 +95,6 @@ private:
     } props;
 
     bool rxEnabled;
-
-    /**
-     * This method is called by `static streamCallback()`. Do not call manually.
-     *
-     * @param buffer
-     * @param length
-     * @param progress
-     * @param userdata
-     * @return int
-     */
-    int streamCallback(uint8_t *buffer, int length, FTDIProgressInfo *progress);
 };
 
 } // namespace ftdi
--- src/uart.cpp
+++ src/uart.cpp
@@ -4,66 +4,22 @@
 namespace firm {
 namespace ftdi {
 
-int FTDI::streamCallback(uint8_t *buffer, int length, FTDIProgressInfo *progress, void *userdata) {
-    if (userdata == NULL) {
-        return -1;
-    }
-
-    FTDI *instance = (FTDI *)userdata;
-    return instance->streamCallback(buffer, length, progress);
-}
-
-int FTDI::streamCallback(uint8_t *buffer, int length, FTDIProgressInfo *progress) {
-    // We can abort the stream by returning a non-zero value
-    if (!this->rxEnabled) {
-        return -1;
-    }
-
-    if (this->handler == NULL) {
-        return -1;
-    }
-
-    // First check if there is a break condition or parity/framing error
-    uint16_t status;
-    int err = ftdi_poll_modem_status(&this->ctx, &status);
-    if (err < 0) {
-        this->log->error("[FTDI] Failed to poll modem status: %s", ftdi_get_error_string(&this->ctx));
-        return -1;
-    }
-    if (status & (1 << (8 + 2))) {
-        this->handler->uartSignal(firm::uart::ERR_PARITY);
-    }
-    if (status & (1 << (8 + 3))) {
-        this->handler->uartSignal(firm::uart::ERR_FRAME);
-    }
-    if (status & (1 << (8 + 4))) {
-        this->handler->uartSignal(firm::uart::BREAK);
-    }
-
-    // Pass data to downstream handler
-    for (int i = 0; i < length; i++) {
-        this->handler->uartData(buffer[i]);
-    }
-
-    return 0;
-}
-
 int FTDI::open(uint16_t vid, uint16_t pid) {
     return ftdi_usb_open(&this->ctx, vid, pid);
 }
 
 int FTDI::configure(firm::uart::Config_t config) {
+    // Remember the configuration under this->props because we will need it later
+    // when calling ftdi_set_line_property2 to set and clear the break condition
     switch (config.dataBits) {
         case firm::uart::DATA_BITS_7: this->props.bits = BITS_7; break;
         case firm::uart::DATA_BITS_8: this->props.bits = BITS_8; break;
         default: return -1;
     }
-
     switch (config.stopBits) {
         case firm::uart::STOP_BITS_1: this->props.stopbits = STOP_BIT_1; break;
         case firm::uart::STOP_BITS_2: this->props.stopbits = STOP_BIT_2; break;
     }
-
     switch (config.parity) {
         case firm::uart::PARITY_NONE: this->props.parity = NONE; break;
         case firm::uart::PARITY_ODD: this->props.parity = ODD; break;
@@ -76,6 +32,12 @@ int FTDI::configure(firm::uart::Config_t config) {
         return err;
     }
 
+    ftdi_read_data_set_chunksize(&this->ctx, this->rxBufferSize);
+    ftdi_set_latency_timer(&this->ctx, 1);
+    ftdi_setflowctrl(&this->ctx, SIO_DISABLE_FLOW_CTRL);
+    ftdi_set_bitmode(&this->ctx, 0xFF, BITMODE_RESET);
+    ftdi_tcioflush(&this->ctx);
+
     return 0;
 }
 
@@ -86,14 +48,47 @@ void FTDI::setBaudRate(uint32_t rate) {
     }
 }
 
-void FTDI::enableRx(bool enabled) {
-    if (!this->rxEnabled && enabled) {
-        int err = ftdi_readstream(&this->ctx, FTDI::streamCallback, this, 2, 4);
-        if (err < 0) {
-            this->log->error("[FTDI] Failed to start read stream: %s", ftdi_get_error_string(&this->ctx));
-        }
+void FTDI::poll() {
+    if (!this->rxEnabled) {
+        return;
     }
 
+    if (this->handler == NULL) {
+        return;
+    }
+
+    // First check if there is a break condition or parity/framing error
+    uint16_t status;
+    int err = ftdi_poll_modem_status(&this->ctx, &status);
+    if (err < 0) {
+        this->log->error("[FTDI] Failed to poll modem status: %s", ftdi_get_error_string(&this->ctx));
+        return;
+    }
+    if (status & (1 << (8 + 2))) {
+        this->handler->uartSignal(firm::uart::ERR_PARITY);
+    }
+    if (status & (1 << (8 + 3))) {
+        this->handler->uartSignal(firm::uart::ERR_FRAME);
+    }
+    if (status & (1 << (8 + 4))) {
+        this->handler->uartSignal(firm::uart::BREAK);
+    }
+
+    // Check for new data
+    int read = ftdi_read_data(&this->ctx, this->rxBuffer, this->rxBufferSize);
+    if (read < 0) {
+        this->log->error("[FTDI] Failed to read data: %s", ftdi_get_error_string(&this->ctx));
+        return;
+    }
+
+    for (int i = 0; i < read; i++) {
+        this->handler->uartData(this->rxBuffer[i]);
+    }
+
+    return;
+}
+
+void FTDI::enableRx(bool enabled) {
     this->rxEnabled = enabled;
 }
 
