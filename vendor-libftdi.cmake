8e82d9cc1f2590dcc18ed1d27c5348433f88d75d
Joseph Paul <joseph@sehrgute.software>
2024-03-15 14:58:31 +0100

    Include libftdi using FetchContent instead of git submodule

--- vendor-libftdi.cmake
+++ vendor-libftdi.cmake
@@ -1,2 +1,7 @@
-add_subdirectory(${PROJECT_SOURCE_DIR}/libftdi)
-include(libftdi/cmake/UseLibFTDI1.cmake)
+message(STATUS "Fetching libftdi...")
+FetchContent_Declare(
+  libftdi
+  GIT_REPOSITORY "https://github.com/sehrgutesoftware/libftdi"
+  GIT_TAG "e8c49df"
+)
+FetchContent_MakeAvailable(libftdi)
